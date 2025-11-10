68214e6e4c1533fa286dd7824b34f81a3845a3ab
justenes1 <emiemir290@gmail.com>
2025-11-10 07:32:07 -0500

    keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,10 +1,17 @@
-const express = require("express");
+// keep_alive.js
+const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Bot is alive!");
+app.get('/', (req, res) => {
+  res.send('Bot is alive!');
 });
 
 app.listen(3000, () => {
-  console.log("🌐 Server is running and keeping bot alive.");
+  console.log('Keep-alive server is running!');
 });
+
+// Ping itself every 1 minute
+setInterval(() => {
+  fetch('http://localhost:3000').catch(() => console.log('Keep-alive ping failed.'));
+}, 60 * 1000);
